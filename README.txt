a6fd02c5904f40e0e30ae6f0d5dd305289ad0bb2
Ramiro Gallo <ramiro.gallo.code@gmail.com>
2024-03-10 09:49:02 -0300

    Creación de repo y primer commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+incluir mas formulas mediante arrays de objetos.
+
+Harris-Benedict Equation
+men: BMR = (13.75 × weight [kg]) + (5.003 × height [cm]) – (6.775 × age [years]) + 66.5
+women: BMR = (9.563 × weight [kg]) + (1.850 × height [cm]) – (4.676 × age [years]) + 655.1
+
+Revised Harris-Benedict Equation
+men: BMR = (13.397 × weight [kg]) + (4.799 × height [cm]) – ( 5.677 × age [years]) + 88.362
+women: BMR = (9.247 × weight [kg]) + (3.098 × height [cm]) – (4.330 × age [years]) + 447.593
+
+Mifflin St Jeor Equation
+men: BMR = (10 × weight [kg]) + (6.25 × height [cm]) – (5 × age [years]) + 5
+women: BMR = (10 × weight [kg]) + (6.25 × height [cm]) – (5 × age [years]) – 161
+
---
+++ js/main.js
@@ -0,0 +1,80 @@
+// código botón modo oscuro: 
+
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#darkModeToggle");
+
+const enableDarkMode = () => {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", null);
+}
+
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled"){
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+})
+
+// declaración de datos de fórmulas: 
+
+const formulas = [
+    {name: "harris-benedict-men",
+    weightMultiplier: 13.75 ,
+    heightMultiplier: 5.003 ,
+    ageMultiplier: 6.775 , 
+    resto: 66.5 ,
+    },
+    {name: "harris-benedict-women",
+    weightMultiplier: 9.563 ,
+    heightMultiplier: 1.850 ,
+    ageMultiplier: 4.676 , 
+    resto: 655.1 ,
+    },
+    {name: "harris-benedict-revised-men",
+    weightMultiplier: 13.397 ,
+    heightMultiplier: 4.799 ,
+    ageMultiplier: 5.677 , 
+    resto: 88.362 ,
+    },
+    {name: "harris-benedict-revised-women",
+    weightMultiplier: 9.247 ,
+    heightMultiplier: 3.098 ,
+    ageMultiplier: 4.330 , 
+    resto: 447.593 ,
+    },
+    {name: "mifflin-st-jeor-men",
+    weightMultiplier: 10 ,
+    heightMultiplier: 6.25 ,
+    ageMultiplier: 5 , 
+    resto: 5 ,
+    },
+    {name: "mifflin-st-jeor-women",
+    weightMultiplier: 10 ,
+    heightMultiplier: 6.25 ,
+    ageMultiplier: 5 , 
+    resto: -161 ,
+    },
+]
+
+// testeo variables usuario:
+
+let weight = 80;
+let height = 180;
+let age = 22;
+let selectedFormula = 1;
+
+// función de cálculo:
+
+function CalcBMR() {
+    let BMR = (weight*(formulas[selectedFormula].weightMultiplier)) + (height*(formulas[selectedFormula].heightMultiplier)) - (age*(formulas[selectedFormula].ageMultiplier)) + (formulas[selectedFormula].resto);
+    return BMR;    
+}
+console.log(CalcBMR());
+
