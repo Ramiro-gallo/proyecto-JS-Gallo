2fa1877578a7d68f76a06199b0446a06e9042857
Ramiro Gallo <ramiro.gallo.code@gmail.com>
2024-03-12 18:47:55 -0300

    Finalizado de linkeo con DOM y agregado de funciones pertinentes. APP funcional.

--- js/main.js
+++ js/main.js
@@ -68,17 +68,6 @@ const formulas = [
     },
 ]
 
-
-
-// declaración variables usuario:
-
-let selectedFormula = 1 ;
-
-
-
-// función de cálculo:
-
-
 // linkeo variables con DOM:
 
 const formulario = document.getElementById("form");
@@ -86,24 +75,75 @@ const formulario = document.getElementById("form");
 formulario.addEventListener('submit', function(e){
     e.preventDefault();
     
-    let formula = document.getElementById('equationSelect').value;
-    
-    let gender = document.getElementById('genderSelect').value;
+    //linkeo del select de formula con numero de objetos del array de formulas:
 
+    let selectedFormula;
+
+    const formulaSelect = document.getElementById("equationSelect").value;
+
+    const gender = document.getElementById('genderSelect').value;
+
+    if(formulaSelect === "harris-benedict" && gender === "masculino"){
+        selectedFormula = 0;
+    } else if (formulaSelect === "harris-benedict" && gender === "femenino"){
+        selectedFormula = 1;
+    } else if (formulaSelect === "harris-benedict-r" && gender === "masculino"){
+        selectedFormula = 2;
+    } else if (formulaSelect === "harris-benedict-r" && gender === "femenino"){
+        selectedFormula = 3;
+    } else if (formulaSelect === "mifflin-st-jeor" && gender === "masculino") {
+        selectedFormula = 4;
+    } else if (formulaSelect === "mifflin-st-jeor" && gender === "femenino") {
+        selectedFormula = 5;
+    } else {
+        selectedFormula = "ERR";
+    }
+
+    // linkeo de variables de cálculo de la fórmula con inputs del form:
+    
     let weight = document.getElementById('weightinput').value;
 
     let height = document.getElementById('heightinput').value;
 
     let age = document.getElementById('ageinput').value;
 
+    let activityMultiplier = document.getElementById('activitymultiplierinput').value;
     
+    // funcion de cálculo de BMR:
+
     function calcBMR() {
         let BMR = (weight*(formulas[selectedFormula].weightMultiplier)) + (height*(formulas[selectedFormula].heightMultiplier)) - (age*(formulas[selectedFormula].ageMultiplier)) + (formulas[selectedFormula].resto);
         return BMR;    
     }
 
     let BMR = calcBMR();
-    console.log("BMR:" + BMR);
+
+    //funcion de cálculo de TDEE:
+
+    function calcDailyIntake(){
+        let dailyIntake = (BMR * activityMultiplier); 
+        return dailyIntake;
+    }
+
+    let dailyIntake = Math.round(calcDailyIntake());
+
+    // visibilización de etiqueta <p> con resultados de la fórmula.
+
+    function showResults(){
+    
+        let resultados = document.getElementById("results"); 
+
+        resultados.style.visibility = "visible";
+
+        if(isNaN(dailyIntake)){
+            resultados.innerHTML = ("Por favor, no olvides ingresar todos los datos necesarios ni seleccionar todas las opciones correspondientes para poder completar el cálculo.");
+        } else {
+            resultados.innerHTML = ("Tu gasto total de energía diario (TDEE) es de aproximadamente " + dailyIntake + "kcal.<br>Esto significa que, según los parámetros ingresados, gastas alrededor de " + dailyIntake + " calorías por día. Si tu deseo es mantener tu peso, deberías consumir esta cantidad de calorías diarias.<br>Contrariamente, si quieres bajar de peso, lo ideal sería que consumas entre " + (dailyIntake - 300) + " y " + (dailyIntake - 500) + " calorías por día.<br>Si quieres subir de peso, lo ideal sería que consumas entre " + (dailyIntake + 300) + " y " + (dailyIntake + 500) + " calorías por día.");
+        }
+
+    }
+
+    showResults();
 })
 
 
